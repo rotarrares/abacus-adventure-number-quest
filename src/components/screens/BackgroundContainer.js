9c1d28582fd5c8ab85a938481be043e4fb35a3a3
Rotar Rares <rotarrares95@gmail.com>
2025-03-29 14:41:57 +0200

    Try using CSS-in-JS for BackgroundContainer

--- src/components/screens/BackgroundContainer.js
+++ src/components/screens/BackgroundContainer.js
@@ -1,42 +1,43 @@
 import React from 'react';
 
 const BackgroundContainer = ({ children }) => {
-  const containerStyle = {
-    width: '100vw',
-    height: '100vh',
-    overflow: 'hidden',
-    position: 'relative',
-    backgroundColor: '#f0f8ff', // Light blue background color
-  };
-
-  // Create an overlay div with the background image
-  const backgroundImageStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundImage: 'url(/assets/images/background.png)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    opacity: 0.5, // Make the background semi-transparent
-    zIndex: 0,
-  };
-
-  const contentStyle = {
-    position: 'relative',
-    width: '100%',
-    height: '100%',
-    zIndex: 1,
-  };
-
   return (
-    <div style={containerStyle}>
-      <div style={backgroundImageStyle}></div>
-      <div style={contentStyle}>
+    <div className="background-container">
+      <div className="background-image"></div>
+      <div className="content-container">
         {children}
       </div>
+      
+      <style jsx="true">{`
+        .background-container {
+          width: 100vw;
+          height: 100vh;
+          overflow: hidden;
+          position: relative;
+          background-color: #f0f8ff;
+        }
+        
+        .background-image {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-image: url('/assets/images/background.png');
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+          opacity: 0.5;
+          z-index: 0;
+        }
+        
+        .content-container {
+          position: relative;
+          width: 100%;
+          height: 100%;
+          z-index: 1;
+        }
+      `}</style>
     </div>
   );
 };
