f1d991e6fa6062bb48f74e0e3601e63662bd103b
Rotar Rares <rotarrares95@gmail.com>
2025-03-29 14:42:12 +0200

    Update BackgroundContainer to use separate CSS file

--- src/components/screens/BackgroundContainer.js
+++ src/components/screens/BackgroundContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../styles/BackgroundContainer.css';
 
 const BackgroundContainer = ({ children }) => {
   return (
@@ -7,37 +8,6 @@ const BackgroundContainer = ({ children }) => {
       <div className="content-container">
         {children}
       </div>
-      
-      <style jsx="true">{`
-        .background-container {
-          width: 100vw;
-          height: 100vh;
-          overflow: hidden;
-          position: relative;
-          background-color: #f0f8ff;
-        }
-        
-        .background-image {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background-image: url('/assets/images/background.png');
-          background-size: cover;
-          background-position: center;
-          background-repeat: no-repeat;
-          opacity: 0.5;
-          z-index: 0;
-        }
-        
-        .content-container {
-          position: relative;
-          width: 100%;
-          height: 100%;
-          z-index: 1;
-        }
-      `}</style>
     </div>
   );
 };
