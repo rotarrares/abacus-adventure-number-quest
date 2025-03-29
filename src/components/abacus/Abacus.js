a1c53b7fae76a024de4943068479e40a7027ef8e
Rotar Rares <rotarrares95@gmail.com>
2025-03-29 17:29:25 +0200

    Fix: Add fallback to SimpleAbacus when env var is set

--- src/components/abacus/Abacus.js
+++ src/components/abacus/Abacus.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import AbacusModel from './AbacusModel';
+import SimpleAbacus from './SimpleAbacus';
 import AbacusControls from './AbacusControls';
 import { useGameContext } from '../../context/GameContext';
 import '../../styles/Abacus.css';
@@ -8,7 +9,16 @@ import '../../styles/Abacus.css';
 const Abacus = ({ onBeadChange }) => {
   const { gameState } = useGameContext();
   const { abacusState } = gameState;
-
+  
+  // Check if we should use the simple abacus
+  const useSimpleAbacus = process.env.REACT_APP_USE_SIMPLE_ABACUS === 'true';
+  
+  // If using simple abacus, return that component
+  if (useSimpleAbacus) {
+    return <SimpleAbacus onBeadChange={onBeadChange} />;
+  }
+  
+  // Otherwise, return the 3D abacus
   return (
     <div className="abacus-container">
       <div className="abacus-canvas-container">
